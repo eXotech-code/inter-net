9c49a636f8e3e47ffeac7defe0a6e6e6e798d216
Jacob <exotech.code@gmail.com>
2020-03-20 17:18:10 +0100

    Started work on the client JavaScript.

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// use only newest syntax
 (function(){"use strict";
+
+// functions
+function renderMessage() {
+    let message = inputBox.value
+    messageBox.innerHTML = `<p class="message">${message}</p>`
+};
+
+function clear() {
+    inputBox.value = ''
+};
+
+// constants
+const inputBox = document.querySelector('[data-input-box]');
+const messageBox = document.querySelector('[data-message-box]');
+
+// interaction
+inputBox.addEventListener('change', (e) => {
+    renderMessage();
+    clear();
+});
 
 })();
