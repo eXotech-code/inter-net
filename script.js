b7c6057aa3fcb3981f0511c8021f5dd2571b4587
Jacob <exotech.code@gmail.com>
2020-03-20 18:58:56 +0100

    #2 Added a message renderer.

--- script.js
+++ script.js
@@ -2,23 +2,46 @@
 (function(){"use strict";
 
 // functions
-function renderMessage() {
-    let message = inputBox.value
-    messageBox.innerHTML = `<p class="message">${message}</p>`
+function clear() {
+    inputBox.value = '';
 };
 
-function clear() {
-    inputBox.value = ''
+// render array of messages to the messageBox
+function renderMessages(messages) {
+    /*  initialize a string to be rendered
+        to message box as multiple paragraphs */
+    let renderString = ''
+    // add every message from array of messages to renderString
+    for (let i = 0; i < messages.length; i++) {
+        let message = messages[i]
+        // add a new paragraph with an individual message inside
+        renderString += `<p class="message">${message}</p>`;
+        // temporary console log
+        console.log('message currently being rendered: ' + message)
+    };
+    messageBox.innerHTML = renderString;
+    clear();
 };
+
+// get the message from input box and add it to array
+function newMessage() {
+    let message = inputBox.value;
+    messages.push(message);
+    // temporary console log
+    console.log('amount of messages currently in array: ' + messages.length)
+    renderMessages(messages);
+}
 
 // constants
 const inputBox = document.querySelector('[data-input-box]');
 const messageBox = document.querySelector('[data-message-box]');
 
+// variables
+var messages = []
+
 // interaction
 inputBox.addEventListener('change', (e) => {
-    renderMessage();
-    clear();
+    newMessage();
 });
 
 })();
