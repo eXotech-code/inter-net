c0dfa835e0661d48ca251209faedcdd751fa9b9c
Jacob <exotech.code@gmail.com>
2020-03-21 11:33:56 +0100

    #2 JavaScript code cleanup and commands.

--- script.js
+++ script.js
@@ -25,23 +25,70 @@ function renderMessages(messages) {
 
 // get the message from input box and add it to array
 function messageHandler() {
-    let message = inputBox.value;
-    if (message.includes('/admin')) {
+    message = inputBox.value;
+    // commands
+    if (message.includes('/help')) {
+        // this will later be a part of server side code
+        message = 'COMMANDS : /help - show this help screen, /weather - show weather in your location';
+    } else if (message.includes('/weather')) {
+        message = ''
+        weather(message);
+    } else if (message.includes('/admin')) {
         // this will later be a part of server side code
         message = 'DEV : ' + message.replace('/admin', '');
+    } else {
+        message = 'USER-ID : ' + message;   // to be replaced by server side generated user ID
     };
-    messages.push(message);
-    // temporary console log
-    console.log('amount of messages currently in array: ' + messages.length)
-    renderMessages(messages);
+    // check if string contains stuff before adding it
+    if (message) {
+        messages.push(message);
+        // temporary console log
+        console.log('amount of messages currently in array: ' + messages.length);
+        renderMessages(messages);
+    };
 }
+
+// get weather information using DarkSky api
+function weather(message) {
+    let longitude;
+    let latitude;
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            longitude = position.coords.longitude;
+            latitude = position.coords.latitude;
+            // proxy for development purposes
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            // API address to DarkSky servers
+            const api = `${proxy}https://api.darksky.net/forecast/98b6df8c5521254b48809cb362a4dafc/${latitude},${longitude}`;
+            // get weather information from DarkSky servers
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    const {temperature, summary} = data.currently;
+                    message = 'WEATHER : In ' + data.timezone + ' it is ' + temperature + ' degrees with ' + summary + '.';
+                    // temporary fix for a bug
+                    if (message) {
+                        messages.push(message);
+                        // temporary console log
+                        console.log('amount of messages currently in array: ' + messages.length);
+                        renderMessages(messages);
+                    };
+                });
+        });
+    };
+};
 
 // constants
 const inputBox = document.querySelector('[data-input-box]');
 const messageBox = document.querySelector('[data-message-box]');
 
 // variables
-var messages = []
+var message = '';
+var messages = [];
 
 // interaction
 inputBox.addEventListener('change', (e) => {
