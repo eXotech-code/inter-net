883837074ce7363622ba74bdcd9da72a185443ad
Jacob <exotech.code@gmail.com>
2020-04-20 10:41:48 +0200

    Added username change prompt.

--- static-files/script.js
+++ static-files/script.js
@@ -92,6 +92,7 @@ function sendMessage() {
         if (username) {
             document.cookie = `username=${username}`;
             console.log(`username changed to ${messageObject.username}`);
+            sendLocal(`you changed your username to ${username}`);
         } else {
             sendLocal("Username cannot be empty. Please choose something else.", "USERNAME");
         }
