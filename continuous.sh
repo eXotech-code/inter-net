c1003a921c1e88d1847572c1fbdeea7c4dcad627
Jakub Piskiewicz <jakub@piskiewicz.org>
2020-05-19 10:48:13 +0200

    Fixed double dcoker build

--- continuous.sh
+++ continuous.sh
@@ -8,7 +8,6 @@ git pull
 # rebuild docker image
 docker stop $(docker ps -a -q)
 docker build -t inter-net .
-docker run -p 8080:8080 -d inter-net
 
 # start docker image
 docker run -p 8080:8080 -d inter-net
